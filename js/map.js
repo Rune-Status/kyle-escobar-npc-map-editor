b97734734eac10d085aaeaf2110726c7ec82854d
Kyle Escobar <kyleescobar@vulten.net>
2019-06-03 17:28:50 -0400

    Uploaded data.

--- js/map.js
+++ js/map.js
@@ -15,6 +15,9 @@ import {RegionLabelsControl} from './controls/region_labels_control.js';
 import {RegionLookupControl} from './controls/region_lookup_control.js';
 import {TitleLabel} from './controls/title_label.js';
 
+var npcs = [];
+var latlng = null;
+
 $(document).ready(function () {
 
     var map = L.map('map', {
@@ -59,6 +62,7 @@ $(document).ready(function () {
     var prevMouseRect, prevMousePos;
     map.on('mousemove', function(e) {
         var mousePos = Position.fromLatLng(map, e.latlng, map.plane);
+        latlng = e.latlng;
 
         if (prevMousePos !== mousePos) {
 
@@ -72,4 +76,48 @@ $(document).ready(function () {
             prevMouseRect.addTo(map);
         }
     });
+
+    window.addEventListener('contextmenu', function(e) {
+        document.getElementById("addNpcBox").style.display = 'inline';
+        e.preventDefault();
+    });
+
+    window.addEventListener('keydown', function(e) {
+       if(e.code == "Enter") {
+           document.body.innerHTML = JSON.stringify(npcs);
+       }
+    });
+
+
+    document.getElementById("cancelBtn").onclick = function() {
+        document.getElementById("npcid").value = "";
+        document.getElementById("walkradius").value = "";
+        document.getElementById("addNpcBox").style.display = 'none';
+    };
+
+    document.getElementById("addBtn").onclick = function() {
+        var x = Position.fromLatLng(map, latlng, map.plane).x;
+        var z = Position.fromLatLng(map, latlng, map.plane).y;
+        var height = Position.fromLatLng(map, latlng, map.plane).z;
+        var npcobj = {
+            id: '',
+            tile: {
+                x: 0,
+                z: 0,
+                height: 0
+            },
+            walkradius: 0
+        };
+        npcobj.id = document.getElementById("npcid").value;
+        npcobj.tile.x = x;
+        npcobj.tile.z = z;
+        npcobj.tile.height = height;
+        npcobj.walkradius = document.getElementById("walkradius").value;
+
+        npcs[npcs.length] = npcobj;
+
+        Position.fromLatLng(map, latlng, map.plane).toLeaflet(map).addTo(map);
+        document.getElementById("addNpcBox").style.display = 'none';
+    };
 });
+
